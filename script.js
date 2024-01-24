5a1d9f974b4f5e247363eb300ed84b811df6661a
Якименко Дмитро <108049675+TinyLine@users.noreply.github.com>
2024-01-24 13:34:32 +0200

    ms

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-const arr = document.querySelectorAll('img');
+const arr = document.querySelectorAll('img')
 
-const imageObserver = new IntersectionObserver((entries) => { entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                const lazyImage = entry.target
-                lazyImage.src = lazyImage.dataset.src
-            }
-        })
-    });
+const isIntersectingWatcher = () =>{
+    if (entry.isIntersecting) {
+        entry.target.src = entry.target.dataset.src
+        observer.unobserve(entry.target)
     
+}}
+
+const imageObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.src = entry.target.dataset.src
+            observer.unobserve(entry.target)
+        }
+    })
+}, { threshold: 0.1 })
+
+
     arr.forEach((evt) => {
         imageObserver.observe(evt);
     })

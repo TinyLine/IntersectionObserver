00c38624007dc390adb2091fafa6dc00bc64477e
Якименко Дмитро <108049675+TinyLine@users.noreply.github.com>
2024-02-03 15:34:07 +0200

    ms

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const observer = new IntersectionObserver((entries, observer) => {
+
+// Загрузка зображення як тільки воно стає видимим на екрані
+
+//   const lazyLoadImage = (entry, observer) => {
+//     if (entry.isIntersecting) {
+//       const image = entry.target;
+//       image.src = image.dataset.src;
+//       image.classList.add('loaded'); 
+//       observer.unobserve(image);
+//     }
+//   };
+
+  const lazyImages = document.querySelectorAll('.lazy-load');
+  const loadButton = document.getElementById('loadButton'); 
+
+  const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
       lazyLoadImage(entry, observer);
     });
     
-  }, { rootMargin: '0px 0px 50px 0px' });
+  }, );
 
-const lazyImages = document.querySelectorAll('.lazy-load');
-const loadButton = document.getElementById('loadButton');
-
+//   Загрузка зображення при натискання на кнопку
 loadButton.addEventListener('click', () => {
   lazyImages.forEach(image => {
     if (!image.src && image.dataset.src) {

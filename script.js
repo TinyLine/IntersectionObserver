1d1afc50f84649a93c1bd497b5f75b16d88ea524
Якименко Дмитро <108049675+TinyLine@users.noreply.github.com>
2024-02-08 19:22:47 +0200

    ms

--- script.js
+++ script.js
@@ -1,56 +1,71 @@
 
-// Завантаження зображення як тільки воно стає видимим на екрані
+
+// // Завантаження зображення як тільки воно стає видимим на екрані
 
    const lazyLoadImage = (entry, observer) => {
      if (entry.isIntersecting) {
       const image = entry.target;
-     image.src = image.dataset.src;
-    image.classList.add('loaded'); 
-     observer.unobserve(image);
+      image.src = image.dataset.src;
+     image.classList.add('loaded'); 
+     image.removeAttribute('data-src');
+    observer.unobserve(image);
     }
-   };
+    };
+
+
+  
+    
 
   const lazyImages = document.querySelectorAll('.lazy-load');
-  const loadButton = document.getElementById('loadButton');
-  const closeButton = document.getElementById('closeButton');
+   const loadButton = document.getElementById('loadButton');
+   const closeButton = document.getElementById('closeButton');
 
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      lazyLoadImage(entry, observer);
-    });
+   const observer = new IntersectionObserver((entries, observer) => {
+     entries.forEach(entry => {
+       lazyLoadImage(entry, observer);
+     });
     
-  }, );
+   }, );
 
-
+   
+   //lazyImages.forEach(image => {
+   //  observer.observe(image); // Додаємо кожне зображення до IntersectionObserver
+   //});
   
-  // Завантаження зображення коли натиснута кнопка
-  loadButton.addEventListener('click', () => {
-    lazyImages.forEach(image => {
-      if (!image.src) {
-        image.src = image.dataset.src;
-        image.classList.add('loaded');
-      }
-    });
+   // Завантаження зображення коли натиснута кнопка
+   loadButton.addEventListener('click', () => {
+     lazyImages.forEach(image => {       if (!image.src) {
+         image.src = image.dataset.src;
+         image.classList.add('loaded');
+       }
+     });
   });
   // Кнопка яка закриває зображення
-  closeButton.addEventListener('click', () => {
-    lazyImages.forEach(image => {
-      image.src = ''; 
-      image.classList.remove('loaded');
-    });
-  });
+   closeButton.addEventListener('click', () => {
+     lazyImages.forEach(image => {
+       image.src = ''; 
+        image.classList.remove('loaded');
+     });
+   });
   
-  const toggleImages = () => {
-    const allLoaded = Array.from(lazyImages).every(image => image.classList.contains('loaded'));
+   const toggleImages = () => {
+     const allLoaded = Array.from(lazyImages).every(image => image.classList.contains('loaded'));
   
-    if (allLoaded) {
-      loadButton.style.display = 'none';
-      closeButton.style.display = 'inline-block';
-    } else {
-      loadButton.style.display = 'inline-block';
-      closeButton.style.display = 'none';
-    }
-  };
+     if (allLoaded) {
+       loadButton.style.display = 'none';
+       closeButton.style.display = 'inline-block';
+     } else {
+       loadButton.style.display = 'inline-block';
+       closeButton.style.display = 'none';
+     }
+   };
   
-  loadButton.addEventListener('click', toggleImages);
-  closeButton.addEventListener('click', toggleImages);
+   loadButton.addEventListener('click', toggleImages);
+   closeButton.addEventListener('click', toggleImages);
+
+   
+  
+ 
+  
+  
+  
